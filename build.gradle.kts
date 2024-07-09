4218539c98d495b5e225a4d4e9a4ee28754728f5
Ders Sarossi <bono@bono.hu>
2024-07-09 19:27:36 +0300

    Spring Initializr
    - Also some other setup, customizations, settings
    - Add/Setup JaCoCo

---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+
+    id("jacoco")
+}
+
+group = "hu.bono.bigbank"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(22)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    compileOnly("org.projectlombok:lombok")
+
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+configure<JacocoPluginExtension> {
+    toolVersion = property("jacocoVersion").toString()
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED, TestLogEvent.STANDARD_ERROR)
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.SHORT
+    }
+    finalizedBy(tasks.withType<JacocoReport>())
+}
+
+tasks.withType<JacocoReport> {
+    dependsOn(tasks.named<Test>("test"))
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+    }
+}
+
+tasks.withType<JacocoCoverageVerification> {
+    dependsOn(tasks.withType<JacocoReport>())
+    violationRules {
+        rule {
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.7".toBigDecimal()
+            }
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.7".toBigDecimal()
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.7".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.named<DefaultTask>("check") {
+    dependsOn(tasks.withType<JacocoCoverageVerification>())
+}
---
+++ src/test/java/hu/bono/bigbank/dragons/ApplicationTest.java
@@ -0,0 +1,12 @@
+package hu.bono.bigbank.dragons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
