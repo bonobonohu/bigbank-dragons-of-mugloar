0b7f063380f9d8dd40b2ae91522ef5b531936840
Ders Sarossi <bono@bono.hu>
2024-07-11 00:48:10 +0300

    Implement test for GameService

--- src/test/java/hu/bono/bigbank/dragons/game/infrastructure/GameServiceTest.java
+++ src/test/java/hu/bono/bigbank/dragons/game/infrastructure/GameServiceTest.java
@@ -1,5 +1,38 @@
 package hu.bono.bigbank.dragons.game.infrastructure;
 
+import hu.bono.bigbank.dragons.TestUtils;
+import hu.bono.bigbank.dragons.common.domain.GameSession;
+import hu.bono.bigbank.dragons.common.infrastructure.LogWriter;
+import hu.bono.bigbank.dragons.game.application.GameClient;
+import hu.bono.bigbank.dragons.game.application.PostGameStartResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.Instant;
+
 class GameServiceTest {
 
+    private final GameClient gameClient = Mockito.mock(GameClient.class);
+    private final LogWriter logWriter = Mockito.mock(LogWriter.class);
+    private final GameService underTest = new GameService(gameClient, logWriter);
+
+    @BeforeEach
+    void beforeEach() {
+        Mockito.reset(gameClient, logWriter);
+    }
+
+    @Test
+    void testGameStart() {
+        final PostGameStartResponse postGameStartResponse = TestUtils.createPostGameStartResponse("GameId123");
+        final GameSession expected = TestUtils.createGameSession(Instant.now(), "GameId123");
+        Mockito.when(gameClient.postGameStart())
+            .thenReturn(postGameStartResponse);
+        final GameSession actual = underTest.gameStart();
+        Assertions.assertThat(actual.getGameId()).isEqualTo(expected.getGameId());
+        Mockito.verify(gameClient, Mockito.times(1)).postGameStart();
+        Mockito.verify(logWriter, Mockito.times(1))
+            .log(actual, "gameStart", "New game started", postGameStartResponse);
+    }
 }
